d0d803baad7041202673a6d79efbe0649cb61323
s4916759 <s4916759@bournemouth.ac.uk>
2020-05-10 15:53:33 +0100

    Changed fields to calendar

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -8,56 +8,64 @@ export default class Profile extends Component {
 		super();
 		this.state = {
 			user: auth().currentUser,
-			notes: [],
+			calendar: [],
 			content: "",
 		};
 	}
 
 	componentDidMount() {
-		db.ref("all_notes/0001").on("value", (snapshot) => {
-			let allNotes = [];
+		db.ref("calendars/calendarID").on("value", (snapshot) => {
+			let calendar = [];
 			snapshot.forEach((snap) => {
-				allNotes.push(snap.val());
+				calendar.push(snap.val());
 			});
-			this.setState({ notes: allNotes });
+			this.setState({ calendar: calendar });
 		});
 	}
 
-	handleChange = (e) => {
-		this.setState({ content: e.target.value });
-	};
+	// componentDidMount() {
+	// 	db.ref("all_notes/0001").on("value", (snapshot) => {
+	// 		let allNotes = [];
+	// 		snapshot.forEach((snap) => {
+	// 			allNotes.push(snap.val());
+	// 		});
+	// 		this.setState({ notes: allNotes });
+	// 	});
+	// }
 
-	createNote = () => {
-		const uid = this.state.user.uid;
-		const { content } = this.state;
-		const note_id = `note-${Date.now()}`;
+	// handleChange = (e) => {
+	// 	this.setState({ content: e.target.value });
+	// };
 
-		db.ref(`all_notes/${uid}/${note_id}`)
-			.set({
-				content,
-				note_id,
-				uid,
-			})
-			.then((_) => {
-				this.setState({ content: "" });
-			});
-	};
+	// createNote = () => {
+	// 	const uid = this.state.user.uid;
+	// 	const { content } = this.state;
+	// 	const note_id = `note-${Date.now()}`;
+
+	// 	db.ref(`all_notes/${uid}/${note_id}`)
+	// 		.set({
+	// 			content,
+	// 			note_id,
+	// 			uid,
+	// 		})
+	// 		.then((_) => {
+	// 			this.setState({ content: "" });
+	// 		});
+	// };
 
 	render() {
-		console.log(this.state.notes);
+		console.log(this.state.calendar);
 		return (
 			<div>
 				<Header />
 				<div>
 					Logged in as: <strong>{this.state.user.email}</strong>
 				</div>
-				{this.state.notes.map((note) => {
-					return <div key={note.note_id}>{note.content}</div>;
-				})}
-				<div>
+				<div></div>
+				{/* <div>
 					<input onChange={this.handleChange} value={this.state.content} />
 					<button onClick={this.createNote}>Create Note</button>
-				</div>
+				</div> */}
 				<Footer />
 			</div>
 		);
