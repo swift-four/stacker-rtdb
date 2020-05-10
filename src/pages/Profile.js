52cf7d225c49f85f3060cc20d9dd8d495a70b427
s4916759 <s4916759@bournemouth.ac.uk>
2020-05-10 11:55:24 +0100

    Connected to RTDB

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,24 +4,36 @@ import Footer from "../components/Footer";
 import { auth, db } from "../services/firebase";
 
 export default class Profile extends Component {
-  constructor() {
-    super();
-    this.state = {
-      user: auth().currentUser
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			user: auth().currentUser,
+			notes: [],
+		};
+	}
 
-  componentDidMount() {}
+	componentDidMount() {
+		db.ref("all_notes/0001").on("value", (snapshot) => {
+			let allNotes = [];
+			snapshot.forEach((snap) => {
+				allNotes.push(snap.val());
+			});
+			this.setState({ notes: allNotes });
+		});
+	}
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <div>
-          Login in as: <strong>{this.state.user.email}</strong>
-        </div>
-        <Footer />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<Header />
+				<div>
+					Login in as: <strong>{this.state.user.email}</strong>
+				</div>
+				{this.state.notes.map((note) => {
+					return <div key={note.note_id}>{note.content}</div>;
+				})}
+				<Footer />
+			</div>
+		);
+	}
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,14 @@
-import firebase from "firebase";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/database";
+
 const config = {
-  apiKey: "YOUR_DETAILS_HERE",
-  authDomain: "YOUR_DETAILS_HERE",
-  databaseURL: "YOUR_DETAILS_HERE"
+	apiKey: "AIzaSyAdKE8pVi0aKm3wUe73nOmNmDF-uAhbCEg",
+	authDomain: "stacker-rdb.firebaseapp.com",
+	databaseURL: "https://stacker-rdb.firebaseio.com",
 };
 
 firebase.initializeApp(config);
 
 export const auth = firebase.auth;
+export const db = firebase.database();
